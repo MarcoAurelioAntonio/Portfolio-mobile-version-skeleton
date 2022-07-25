bd19bd73bec3b333a7ba99f162f9993089764f47
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-07-25 16:57:15 -0300

    Add ESlint file

--- dom.js
+++ dom.js
@@ -1,9 +1,8 @@
 /* Open Menu in Mobile version */
 const menu = document.getElementById('menu');
-const nav2 = document.querySelector('.navlinks')
-
-menu.addEventListener('click', displayMenu);
+const nav2 = document.querySelector('.navlinks');
 
 function displayMenu() {
-    nav2.classList.toggle('active');
+  nav2.classList.toggle('active');
 }
+menu.addEventListener('click', displayMenu);
